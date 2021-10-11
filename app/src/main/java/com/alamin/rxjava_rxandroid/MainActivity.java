030f4974c09fe60bc619ad08d40d40a2e5c13efa
Al-AminJoy <alaminislam3555@gmail.com>
2021-10-11 16:29:30 +0600

    Create,Range,Repeat Operator

--- app/src/main/java/com/alamin/rxjava_rxandroid/MainActivity.java
+++ app/src/main/java/com/alamin/rxjava_rxandroid/MainActivity.java
@@ -10,13 +10,18 @@ import android.widget.TextView;
 import com.alamin.rxjava_rxandroid.pojo.Task;
 import com.alamin.rxjava_rxandroid.service.DataSource;
 
+import java.util.List;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
@@ -36,6 +41,132 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         textView=findViewById(R.id.textView);
         seekBar=findViewById(R.id.seekBar);
+        //observable();
+        //Operators
+        //createOperator();
+       // rangeOperator();
+        repeatOperator();
+    }
+
+    private void repeatOperator() {
+        Observable<Integer> taskObservable = Observable
+                .range(0,3)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .repeat(3);
+
+        taskObservable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@androidx.annotation.NonNull Disposable disposable) {
+
+            }
+
+            @Override
+            public void onNext(@androidx.annotation.NonNull Integer integer) {
+                Log.d(TAG, "onNext: "+ integer);
+
+            }
+
+            @Override
+            public void onError(@androidx.annotation.NonNull Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void rangeOperator() {
+        Observable<Task> taskObservable = Observable
+                .range(0,9)
+                .map(new Function<Integer, Task>() {
+                    @Override
+                    public Task apply(@androidx.annotation.NonNull Integer integer) throws Exception {
+                        return new Task("Task By Map With Priority "+String.valueOf(integer),true,integer);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@androidx.annotation.NonNull Disposable disposable) {
+
+            }
+
+            @Override
+            public void onNext(@androidx.annotation.NonNull Task task) {
+                Log.d(TAG, "onNext: "+ task.getDescription());
+
+            }
+
+            @Override
+            public void onError(@androidx.annotation.NonNull Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void createOperator() {
+       //Task task = new Task("Take out the trash", true, 3);
+
+        List<Task> tasks = DataSource.createTasksList();
+
+        Observable<Task> taskObservable = Observable
+                .create(new ObservableOnSubscribe<Task>() {
+                    @Override
+                    public void subscribe(@androidx.annotation.NonNull ObservableEmitter<Task> emitter) throws Exception {
+                     //For Single Object
+                      /*  if (!emitter.isDisposed()){
+                            emitter.onNext(task);
+                            emitter.onComplete();
+                        }*/
+
+                        //For List Of Objects
+                        for (Task task : tasks){
+                            if (!emitter.isDisposed()){
+                                emitter.onNext(task);
+                            }
+                        }
+                        if (!emitter.isDisposed()){
+                            emitter.onComplete();
+                        }
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(@androidx.annotation.NonNull Disposable disposable) {
+
+            }
+
+            @Override
+            public void onNext(@androidx.annotation.NonNull Task task) {
+                Log.d(TAG, "onNext: "+ task.getDescription());
+            }
+
+            @Override
+            public void onError(@androidx.annotation.NonNull Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void observable() {
         Observable<Task> taskObservable=Observable
                 .fromIterable(DataSource.createTasksList())
                 .subscribeOn(Schedulers.io())
